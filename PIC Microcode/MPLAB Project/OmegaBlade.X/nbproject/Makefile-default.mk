36b78386a340de1812fabcd61505c1bbde4f1d6a
Ben Jespersen <ben.j.jespersen@gmail.com>
2015-10-29 00:03:59 -0500

    Added IR transmission code to master branch.

--- PIC Microcode/MPLAB Project/OmegaBlade.X/nbproject/Makefile-default.mk
+++ PIC Microcode/MPLAB Project/OmegaBlade.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../../source/main.c ../../source/uart.c ../../source/interrupt.c
+SOURCEFILES_QUOTED_IF_SPACED=../../source/main.c ../../source/uart.c ../../source/interrupt.c ../../source/ir.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/870914629/main.p1 ${OBJECTDIR}/_ext/870914629/uart.p1 ${OBJECTDIR}/_ext/870914629/interrupt.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/870914629/main.p1.d ${OBJECTDIR}/_ext/870914629/uart.p1.d ${OBJECTDIR}/_ext/870914629/interrupt.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/870914629/main.p1 ${OBJECTDIR}/_ext/870914629/uart.p1 ${OBJECTDIR}/_ext/870914629/interrupt.p1 ${OBJECTDIR}/_ext/870914629/ir.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/870914629/main.p1.d ${OBJECTDIR}/_ext/870914629/uart.p1.d ${OBJECTDIR}/_ext/870914629/interrupt.p1.d ${OBJECTDIR}/_ext/870914629/ir.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/870914629/main.p1 ${OBJECTDIR}/_ext/870914629/uart.p1 ${OBJECTDIR}/_ext/870914629/interrupt.p1
+OBJECTFILES=${OBJECTDIR}/_ext/870914629/main.p1 ${OBJECTDIR}/_ext/870914629/uart.p1 ${OBJECTDIR}/_ext/870914629/interrupt.p1 ${OBJECTDIR}/_ext/870914629/ir.p1
 
 # Source Files
-SOURCEFILES=../../source/main.c ../../source/uart.c ../../source/interrupt.c
+SOURCEFILES=../../source/main.c ../../source/uart.c ../../source/interrupt.c ../../source/ir.c
 
 
 CFLAGS=
@@ -105,6 +105,14 @@ ${OBJECTDIR}/_ext/870914629/interrupt.p1: ../../source/interrupt.c  nbproject/Ma
 	@-${MV} ${OBJECTDIR}/_ext/870914629/interrupt.d ${OBJECTDIR}/_ext/870914629/interrupt.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/870914629/interrupt.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/_ext/870914629/ir.p1: ../../source/ir.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/870914629" 
+	@${RM} ${OBJECTDIR}/_ext/870914629/ir.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/870914629/ir.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/870914629/ir.p1  ../../source/ir.c 
+	@-${MV} ${OBJECTDIR}/_ext/870914629/ir.d ${OBJECTDIR}/_ext/870914629/ir.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/870914629/ir.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/_ext/870914629/main.p1: ../../source/main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/870914629" 
@@ -129,6 +137,14 @@ ${OBJECTDIR}/_ext/870914629/interrupt.p1: ../../source/interrupt.c  nbproject/Ma
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/870914629/interrupt.p1  ../../source/interrupt.c 
 	@-${MV} ${OBJECTDIR}/_ext/870914629/interrupt.d ${OBJECTDIR}/_ext/870914629/interrupt.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/_ext/870914629/interrupt.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/_ext/870914629/ir.p1: ../../source/ir.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}/_ext/870914629" 
+	@${RM} ${OBJECTDIR}/_ext/870914629/ir.p1.d 
+	@${RM} ${OBJECTDIR}/_ext/870914629/ir.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/870914629/ir.p1  ../../source/ir.c 
+	@-${MV} ${OBJECTDIR}/_ext/870914629/ir.d ${OBJECTDIR}/_ext/870914629/ir.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/_ext/870914629/ir.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
---
+++ PIC Microcode/source/ir.c
@@ -0,0 +1,119 @@
+#include "main_header.h"
+
+void init_ir()
+{
+    PSMC1CON  = 0x00;
+	PSMC1MDL  = 0x80;
+	PSMC1SYNC = 0x00;
+	PSMC1CLK  = 0x00;
+	PSMC1POL  = 0x00;
+	PSMC1BLNK = 0x00;
+	PSMC1REBS = 0x00;
+	PSMC1FEBS = 0x00;
+	PSMC1PHS  = 0x01;
+	PSMC1DCS  = 0x01;
+	PSMC1PRS  = 0x01;
+	PSMC1ASDC = 0x00;
+	PSMC1ASDL = 0x08;
+	PSMC1ASDS = 0x00;
+	PSMC1PHH  = 0x00;
+	PSMC1PHL  = 0x00;
+	PSMC1DCH  = 0x01;
+	PSMC1DCL  = 0x6A;
+	PSMC1PRH  = 0x02;
+	PSMC1PRL  = 0x3A;
+	PSMC1DBR  = 0x00;
+	PSMC1DBF  = 0x00;
+	PSMC1FFA  = 0x00;
+	PSMC1BLKR = 0x00;
+	PSMC1BLKF = 0x00;
+	PSMC1STR0 = 0x08;
+	PSMC1STR1 = 0x00;
+	PSMC1INT  = 0x00;
+	PSMC1OEN  = 0x08;
+	PSMC1CON  = 0x80;
+	PIE4     &= 0xEE;
+	PIE4     |= 0x00;
+}
+
+void output_ir(char pkt_type, char amount)
+{
+	if(pkt_type == 0)
+	{
+		for(int i = 0;i < amount;i++){
+			//Start Envelope
+			P1MDLBIT = 1;
+			__delay_us(178);
+			P1MDLBIT = 0;
+			__delay_us(2678-178);
+			
+			//2x Damage Envelope
+			P1MDLBIT = 1;
+			__delay_us(357);
+			P1MDLBIT = 0;
+			__delay_us(2678-357);
+			P1MDLBIT = 1;
+			__delay_us(357);
+			P1MDLBIT = 0;
+			__delay_us(2678-357);
+			
+			//Stop envelope
+			P1MDLBIT = 1;
+			__delay_us(2678);
+			P1MDLBIT = 0;
+		}
+		
+	}
+	else if(pkt_type == 1)
+	{
+		for(int i = 0;i < amount;i++){		
+			//Start Envelope
+			P1MDLBIT = 1;
+			__delay_us(178);
+			P1MDLBIT = 0;
+			__delay_us(2678-178);
+			
+			//2x Healing Envelope
+			P1MDLBIT = 1;
+			__delay_us(535);
+			P1MDLBIT = 0;
+			__delay_us(2678-535);
+			P1MDLBIT = 1;
+			__delay_us(535);
+			P1MDLBIT = 0;
+			__delay_us(2678-535);
+			
+			//Stop envelope
+			P1MDLBIT = 1;
+			__delay_us(2678);
+			P1MDLBIT = 0;
+		}
+	}
+	else
+	{
+		for(int i = 0;i < amount;i++){
+			//Start Envelope
+			P1MDLBIT = 1;
+			__delay_us(178);
+			P1MDLBIT = 0;
+			__delay_us(2678-178);
+			
+			//2x Stun Envelope
+			P1MDLBIT = 1;
+			__delay_us(714);
+			P1MDLBIT = 0;
+			__delay_us(2678-714);
+			P1MDLBIT = 1;
+			__delay_us(714);
+			P1MDLBIT = 0;
+			__delay_us(2678-714);
+			
+			//Stop envelope
+			P1MDLBIT = 1;
+			__delay_us(2678);
+			P1MDLBIT = 0;
+		}
+	}
+}
+
+
